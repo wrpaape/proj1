9a39b0af7e9371cf9ef99639f6fdf3f197fa3a7a
Reid Paape <wrpaape@gmail.com>
2015-06-02 18:52:25 -0500

    nightmare mode completed tentatively

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def show # Methods are called Actions in Controllers
-
-
     if params[:first_name] || params[:last_name] || params[:age]
       user_matches = User.new
       response, response_code = user_matches.get(params)
@@ -14,6 +12,11 @@ class UsersController < ApplicationController
   end
 
   def index
+    if params[:id] == "create"
+      params[:method] = params[:id]
+      params.delete(:id)
+      return method
+    end
     user = User.new
     response, response_code = user.get(params)
     render text: response, status: response_code
@@ -30,14 +33,31 @@ class UsersController < ApplicationController
         response << "-" * 50
         response << "user#{params[:id]} Succesfully Deleted"
         response << "-" * 50
-        response << "Response Code: #{@response_code}"
+        response << "Response Code: #{response_code}"
         response << "-" * 50
       else
         response_code = 404
         response << "-" * 50
         response << "Not Found LOL"
         response << "-" * 50
-        response << "Response Code: #{@response_code}"
+        response << "Response Code: #{response_code}"
+        response << "-" * 50
+      end
+    when "create"
+      if existing_user = User.find_by(first_name: params[:first_name], last_name: params[:last_name], age: params[:age].to_i)
+        response_code = 500
+        response << "-" * 50
+        response << "user#{existing_user.id} Already Exists"
+        response << "-" * 50
+        response << "Response Code: #{response_code}"
+        response << "-" * 50
+      else
+        User.create(first_name: params[:first_name], last_name: params[:last_name], age: params[:age].to_i)
+        id = User.last.id
+        response << "-" * 50
+        response << "user#{id} Succesfully Created"
+        response << "-" * 50
+        response << "Response Code: #{response_code}"
         response << "-" * 50
       end
     else
@@ -45,7 +65,7 @@ class UsersController < ApplicationController
       response << "-" * 50
       response << "Not Found LOL"
       response << "-" * 50
-      response << "Response Code: #{@response_code}"
+      response << "Response Code: #{response_code}"
       response << "-" * 50
     end
     response = response.join("<p>")
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/users/:id/:method', to: 'users#method'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
