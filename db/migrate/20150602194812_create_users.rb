444e19ff5ae7e186172e16071f6c97eb2f298af4
Reid Paape <wrpaape@gmail.com>
2015-06-02 14:59:09 -0500

    first commit

---
+++ db/migrate/20150602194812_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.integer :age, default: 0, null: false
+    end
+  end
+end
